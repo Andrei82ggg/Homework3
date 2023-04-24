c421b3e65813ab9ef15d6ed8f03e7d7e3d406c02
Professional <ustu1982@mail.ru>
2023-04-24 21:25:27 +0500

    int

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        byte a=67;
+        System.out.println("Значение переменной a с типом byte равно "+a);
+        int b=27897;
+        System.out.println("Значение переменной b с типом int равно "+b);
+        short c=-159;
+        System.out.println("Значение переменной c с типом short равно "+c);
+        short d=569;
+        System.out.println("Значение переменной d с типом short равно "+d);
+        long e=987678965546L;
+System.out.println("Значение переменной e с типом long равно "+e);
+float i=27.12f;
+System.out.println("Значение переменной e с типом floet равно "+i);
+float k=2.786f;
+System.out.println("Значение переменной e с типом floet равно "+k);
+
+    byte ludmila=23;
+    byte anna=27;
+    byte ekaterina=30;
+    int allTeacher=(ludmila + anna + ekaterina);
+    System.out.println(allTeacher);
+    int listAll=480;
+    float listOneL =listAll/allTeacher;
+    System.out.println("На каждого ученика рассчитано "+listOneL+ "листов бумаги");
+
+    }
+}
