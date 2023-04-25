9b5b89befa34f2e4d2a90407d9f81626e3533d30
Professional <ustu1982@mail.ru>
2023-04-25 21:54:09 +0500

    int

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,64 @@ System.out.println("Значение переменной e с типом floet 
     float listOneL =listAll/allTeacher;
     System.out.println("На каждого ученика рассчитано "+listOneL+ "листов бумаги");
 
+    byte prodmashin=16;
+    System.out.println("Производительность машины за две минуты "+prodmashin+ "бутылок");
+    int twentyMin=prodmashin*10;
+    System.out.println("Производительность машины за двадцать минут "+twentyMin+ "бутылок");
+    int day=24*60/2*prodmashin;
+    System.out.println("Производительность машины за сутки  "+day+ "бутылок");
+    int treeday=day*3;
+    System.out.println("Производительность машины за три дня "+treeday+ "бутылок");
+    int month=day*30;
+    System.out.println("Производительность машины за месяц "+month+ "бутылок");
+
+    int allBank=120;
+    int oneClassWhite=2;
+    int oneClassBrown=4;
+    int allClass=allBank/(oneClassBrown+oneClassWhite);
+    System.out.println("Всего классов "+allClass);
+    int whiteBank=allClass*oneClassWhite;
+    int brownBank=allClass*oneClassBrown;
+    System.out.println("В школе, где "+allClass+"классов, нужно "+whiteBank+"банок белой краски и "+brownBank+"банок коричневой краски");
+
+    int banan=80;
+    int fiveBanan=banan*5;
+    int milkSto=105;
+    int milkTwo=milkSto*2;
+    int ice=100;
+    int iceTwo=ice*2;
+    int egg=70;
+    int eggFour=egg*4;
+    int weightGram=fiveBanan+milkTwo+iceTwo+eggFour;
+    float weightKilo=weightGram/1000f;
+    System.out.println("Завтрак спортсмена "+weightGram+"грамм "+weightKilo+"килограмм");
+
+    int veight=7;
+    int veightOne=250;
+    int veightTwo=500;
+    int dayOne=veight*1000/veightOne;
+    System.out.println("Уйдет "+dayOne+"если спортсмен будет терять по 250 гр ежедневно");
+    int dayTwo=veight*1000/veightTwo;
+    System.out.println("Уйдет "+dayTwo+"если спортсмен будет терять по 500 гр ежедневно");
+    int averege=(dayOne+dayTwo)/2;
+    System.out.println("Понадобится "+averege+"дней в среднем, чтобы добиться результата похудения.");
+
+    int masha=67760;
+    int denis=83960;
+    int cristina=76230;
+    int mashaUp=masha/100*10+masha;
+        int denisUp=denis/100*10+denis;
+    int cristinaUp=cristina/100*10+cristina;
+    int zp=masha+denis+cristina;
+    int zpUp=mashaUp+denisUp+cristinaUp;
+    int zpYers=zpUp-zp;
+    int difMasha=mashaUp-masha;
+    int difDenis=denisUp-denis;
+    int difCristina=cristinaUp-cristina;
+    System.out.println("Маша теперь получает " +mashaUp+" рублей. Годовой доход вырос на " +difMasha+" рублей");
+    System.out.println("Денис теперь получает " +denisUp+" рублей. Годовой доход вырос на " +difDenis+" рублей");
+    System.out.println("Кристина теперь получает " +cristinaUp+" рублей. Годовой доход вырос на " +difCristina+" рублей");
+
+    
     }
 }
